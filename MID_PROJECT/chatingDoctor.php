d6495d02f5cb6e0047c736ecd3300e2cc00ddb5c
Tuhin003 <62533032+Tuhin003@users.noreply.github.com>
2020-08-13 00:44:51 +0600

    chatingDoctor

---
+++ MID_PROJECT/chatingDoctor.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <fieldset>
+        <center>
+            <legend><b>CHATING WITH DOCTOR</b></legend><br>
+    
+        </center></br>
+
+        <tr>
+            <td>
+                HELLO SIR,
+            </td><br>
+
+            <td>
+                I HAVE A PROBLIM..
+            </td><br>
+            <td>
+                TYPING...........
+            </td></br>
+        </tr>
+     </fieldset>
+
+            
+</body>
+</html>    	
