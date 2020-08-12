ae15f101051c59178197bf1f8c04f92f3407a84d
Tuhin003 <62533032+Tuhin003@users.noreply.github.com>
2020-08-13 00:47:30 +0600

    chatingPatient

---
+++ MID_PROJECT/chatingPatient.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <fieldset>
+        <center>
+            <legend><b>CHATING WITH PATIENT</b></legend><br>
+    
+        </center></br>
+
+        
+        <tr>
+            <td>
+                HELLO MISTER,
+            </td><br>
+
+            <td>
+                HOW CAN I HELP YOU?????
+            </td><br>
+            <td>
+                TYPING...........
+            </td></br>
+        </tr>
+     </fieldset>
+
+            
+</body>
+</html>    	
