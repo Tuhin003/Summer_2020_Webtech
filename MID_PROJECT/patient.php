292ccd5eaeca847234ed6060c803193e2da8c886
Tuhin003 <62533032+Tuhin003@users.noreply.github.com>
2020-08-13 00:34:58 +0600

    patient

---
+++ MID_PROJECT/patient.php
@@ -0,0 +1,70 @@
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <fieldset>
+    	 <center>
+        <img src="patient.jpg"  width="800" height="500">
+        </center></br>
+        <center>
+            <legend><b>VIEW PATIENT LIST</b></legend>
+           
+        </center>
+
+         <a href="searchPatient.php"><u>SEARCH LIST</a><br>
+         <a href="chatingPatient.php"><u>CHATING WITH PATIENT</a></br>   
+
+    <center>
+        <table border="1">
+            <tr>
+                <td>ID</td>
+                <td>NAME</td>
+                <td>EMAIL</td>
+                <td>ADDRESS</td>
+                <td>DATE OF BIRTH</td>
+                <td>DISEASE</td>
+                <td>PHONE</td>
+            </tr>
+
+            <tr>
+                <td>18-36391-1</td>
+                <td>Parthib</td>
+                <td>tgop@gmail.com</td>
+                <td>MIRPUR</td>
+                <td>16-07-1999</td>
+                <td>HEART PROBLEM</td>
+                <td>01703307789</td>
+            </tr>
+
+            <tr>
+                <td>18-32335-1</td>
+                <td>SIAM</td>
+                <td>siam@gmail.com</td>
+                <td>DHANMONDI</td>
+                <td>02-04-1998</td>
+                <td>KIDNY STONE</td>
+                <td>01673003556</td>
+            </tr>
+
+            <tr>
+                <td>17-33251-1</td>
+                <td>NILOY</td>
+                <td>NILOY@gmail.com</td>
+                <td>GAZIPUR</td>
+                <td>24-05-2020</td>
+                <td>COLD FEVER</td>
+                <td>01884115561</td>
+            </tr>
+
+            
+        </table>
+        
+        
+
+    </center><br>
+
+    </fieldset>
+            
+</body>
+</html>
