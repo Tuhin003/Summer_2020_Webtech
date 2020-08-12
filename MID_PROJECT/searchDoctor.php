2f8ace239791183bc88e73e474f62b037e3a5b8e
Tuhin003 <62533032+Tuhin003@users.noreply.github.com>
2020-08-13 00:37:26 +0600

    searchDoctor

---
+++ MID_PROJECT/searchDoctor.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <fieldset>
+        <center>
+            <legend><b>VIEW DOCTOR LIST</b></legend>
+           
+        </center>
+        <tr>
+        	<tr>
+				<td>Search</td>
+				<td>:</td>
+				<td><input name="name" type="text"></td>
+				<input name="submit" type="submit" value="Search">
+				<td></td>
+			</tr>
+        </tr>
+
+
+        <table border="1">
+            <tr>
+                <td>ID</td>
+                <td>NAME</td>
+                <td>EMAIL</td>
+                <td>ADDRESS</td>
+                <td>DATE OF BIRTH</td>
+                <td>SPECIALIST</td>
+                <td>PHONE</td>
+            </tr>
+
+            <tr>
+                <td>1</td>
+                <td>TUHIN</td>
+                <td>tuhin@gmail.com</td>
+                <td>MIRPUR</td>
+                <td>25-08-1996</td>
+                <td>HEART</td>
+                <td>01703302545</td>
+            </tr>
+        </table> 
+        </center><br>
+
+    </fieldset>
+            
+</body>
+</html>   
