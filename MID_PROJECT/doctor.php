fd030586258fef71d508ef019efdb25a60d2797b
Tuhin003 <62533032+Tuhin003@users.noreply.github.com>
2020-08-13 00:31:35 +0600

    doctor

---
+++ MID_PROJECT/doctor.php
@@ -0,0 +1,70 @@
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <fieldset>
+        <center>
+        <img src="doctor.jpg"  width="800" height="500">
+        </center></br>
+        <center>
+            <legend><b>VIEW DOCTOR LIST</b></legend>
+           
+        </center>
+
+         <a href="searchDoctor.php"><u>SEARCH LIST</a><br>
+         <a href="chatingDoctor.php"><u>CHATING WITH DOCTOR</a></br>   
+
+    <center>
+        <table border="1">
+            <tr>
+                <td>ID</td>
+                <td>NAME</td>
+                <td>EMAIL</td>
+                <td>ADDRESS</td>
+                <td>DATE OF BIRTH</td>
+                <td>SPECIALIST</td>
+                <td>PHONE</td>
+            </tr>
+
+            <tr>
+                <td>1</td>
+                <td>TUHIN</td>
+                <td>tuhin@gmail.com</td>
+                <td>MIRPUR</td>
+                <td>25-08-1996</td>
+                <td>HEART</td>
+                <td>01703302545</td>
+            </tr>
+
+            <tr>
+                <td>2</td>
+                <td>TUSAR</td>
+                <td>tusar@gmail.com</td>
+                <td>DHANMONDI</td>
+                <td>02-04-1989</td>
+                <td>KIDNY</td>
+                <td>01673003556</td>
+            </tr>
+
+            <tr>
+                <td>3</td>
+                <td>MUNIA</td>
+                <td>munia@gmail.com</td>
+                <td>GAZIPUR</td>
+                <td>24-05-2020</td>
+                <td>CHILD SPECIALIST</td>
+                <td>01884112123</td>
+            </tr>
+
+            
+        </table>
+        
+        
+
+    </center><br>
+
+    </fieldset>
+            
+</body>
+</html>
