bccf97dd6a1e3ad7ab8c05817bed88b2dc071918
Tuhin003 <62533032+Tuhin003@users.noreply.github.com>
2020-08-13 00:39:19 +0600

    searchPatient

---
+++ MID_PROJECT/searchPatient.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <fieldset>
+        <center>
+            <legend><b>VIEW PATIENT LIST</b></legend>
+           
+        </center>
+        <tr>
+        	<tr>
+				<td>Search</td>
+				<td>:</td>
+				<td><input name="name" type="text"></td>
+				<input name="submit" type="submit" value="Search">
+				<td></td>
+			</tr>
+        </tr>
+
+
+        <table border="1">
+            <tr>
+                <td>ID</td>
+                <td>NAME</td>
+                <td>EMAIL</td>
+                <td>ADDRESS</td>
+                <td>DATE OF BIRTH</td>
+                <td>DISEASE</td>
+                <td>PHONE</td>
+            </tr>
+
+            <tr>
+                <td>18-36391-1</td>
+                <td>Parthib</td>
+                <td>tgop@gmail.com</td>
+                <td>MIRPUR</td>
+                <td>16-07-1999</td>
+                <td>HEART PROBLEM</td>
+                <td>01703307789</td>
+            </tr>
+        </table> 
+        </center><br>
+
+    </fieldset>
+            
+</body>
+</html>       
