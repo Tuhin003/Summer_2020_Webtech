8c7e8e1b62977842a720f7c5751edddd9a006b01
Tuhin003 <62533032+Tuhin003@users.noreply.github.com>
2020-09-01 09:24:29 +0600

    Update register.php

--- FINAL_LAB_TASK_4/views/register.php
+++ FINAL_LAB_TASK_4/views/register.php
@@ -16,108 +16,88 @@
 </head>
 <body>
 
-	<form action="../php/regCheck.php" method="post" onsubmit="return validate()">
+	<form action="../php/regCheck.php" method="post">
 		<fieldset>
 			<legend>SignUp</legend>
 			<table>
 				<tr>
 					<td>Username</td>
-					<td><input type="text" name="username" id="username" onkeyup="removername()"></td>
-					<td id="namemsg"></td>
-				</tr>
-				<tr>
-					<td>Email</td>
-					<td><input type="text" name="email" id="email" onkeyup="removeremail()"></td>
-					<td id="emailmsg"></td>
+					<td><input type="text" id="name" name="username"></td>
 				</tr>
 				<tr>
 					<td>Password</td>
-					<td><input type="password" name="password" id="password" onkeyup="removerpassword()"></td>
-					<td id="passwordmsg"></td>
+					<td><input type="password" id="password" name="password"></td>
+				</tr>
+				<tr>
+					<td>Email</td>
+					<td><input type="text" id="email" name="email" onkeyup="f2()"></td>
+					<td id="emailmsg"></td>
 				</tr>
 				<tr>
 					<td></td>
-					<td><input type="button" name="submit" value="Submit" onkeyup="f1()"></td>
-					<a href="login.php" style="display: none">login</a>
+					<td><input type="button" name="submit" value="Submit" onclick="f1()">
+						<a href="login.php" style="display: none">Login</a></td>
 				</tr>
 			</table>
 		</fieldset>
 	</form>
+
 	<script type="text/javascript">
-		function f1()
-		{
-			document.getElementaByTagName('a')[0].style.display='inline';
-		}
-		function validate()
-		{
-			var validname=validatename();
-			var validemail=validateemail();
-			var validpasssword=validatepassword();
-			if(validname==true && validemail==true && validpassword==true)
-			{
-				return true;
-			}
-			else
-			{
-				return false;
-			}
-		}
-		//name
-		function validatename()
-		{
-			var name=document.getElementById('username').value;
-			if (name=="") 
-			{
-				document.getElementById('namemsg').innerHTML="UserName can not be empty";
-				return false;
-			}
-			else
-			{
-				return true;
-			}	
-			function removername()
-			{
-				document.getElementById('namemsg').innerHTML="";
-			}
-		}
-		//email
-		function validateemail()
-		{
+		function f1() {
+			
+			var name=document.getElementById('email').value;
 			var email=document.getElementById('email').value;
-			if (email=="") 
+			var password=document.getElementById('email').value;
+
+			if(name!="" && email!="" && password!="" && document.getElementById('emailmsg').innerHTML=="")
 			{
-				document.getElementById('emailmsg').innerHTML="Email can not be empty";
-				return false;
-			}
-			else
-			{
-				return true;
+				var xhttp = new XMLHttpRequest();
+			xhttp.open('POST', '../php/regCheck.php', true);
+			xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+			xhttp.send('name='+name+'&passowrd='+password+'&email='+email+);
+
+			xhttp.onreadystatechange = function (){
+			if(this.readyState == 4 && this.status == 200){
+
+				if(this.responseText != ""){
+					document.getElementById('emailmsg').innerHTML = this.responseText;
+				}else{
+					document.getElementById('emailmsg').innerHTML = "";
+				}
+				
 			}	
+			}
+				
+
+
+
+
+				document.getElementsByTagName('a')[0].style.display='inline';
+			}
 		}
 
-		function removeremail()
+		function f2()
 		{
-			document.getElementById('emailmsg').innerHTML="";
-		}
-		//pass
-		function validatepassword()
-		{
-			var password=document.getElementById('password').value;
-			if (password=="") 
-			{
-				document.getElementById('passwordmsg').innerHTML="Password can not be empty";
-				return false;
-			}
-			else
-			{
-				return true;
+			var email = document.getElementById('email').value;
+			var xhttp = new XMLHttpRequest();
+			xhttp.open('POST', '../php/emailCheck.php', true);
+			xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+			xhttp.send('email='+email);
+
+			xhttp.onreadystatechange = function (){
+			if(this.readyState == 4 && this.status == 200){
+
+				if(this.responseText != ""){
+					document.getElementById('emailmsg').innerHTML = this.responseText;
+				}else{
+					document.getElementById('emailmsg').innerHTML = "";
+				}
+				
 			}	
+			}
 		}
 
-		function removerpassword()
-		{
-			document.getElementById('passwordmsg').innerHTML="";
-		}
+
 	</script>
 </body>
 </html>
