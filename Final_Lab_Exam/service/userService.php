0407b87ddea3ad728692e88cd91bccbc1ee2e0ef
Tuhin003 <62533032+Tuhin003@users.noreply.github.com>
2020-09-06 09:07:16 +0600

    usercervice

---
+++ Final_Lab_Exam/service/userService.php
@@ -0,0 +1,105 @@
+<?php
+	require_once('../db/db.php');
+
+	function getByID($id){
+		$conn = dbConnection();
+
+		if(!$conn){
+			echo "DB connection error";
+		}
+
+		$sql = "select * from lab where id={$id}";
+		$result = mysqli_query($conn, $sql);
+		$row = mysqli_fetch_assoc($result);
+		return $row;
+	}
+
+	function getAllUser(){
+		$conn = dbConnection();
+
+		if(!$conn){
+			echo "DB connection error";
+		}
+
+		$sql = "select * from final";
+		$result = mysqli_query($conn, $sql);
+		$users = [];
+
+		while($row = mysqli_fetch_assoc($result)){
+			array_push($users, $row);
+		}
+
+		return $users;
+	}
+
+
+	function validate($user){
+		$conn = dbConnection();
+
+		if(!$conn){
+			echo "DB connection error";
+		}
+
+		$sql = "select * from lab where username='{$user['username']}' and password='{$user['password']}'";
+		$result = mysqli_query($conn, $sql);
+		$user = mysqli_fetch_assoc($result);
+
+		if(count($user) > 0 ){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+
+	function insert($user){
+		$conn = dbConnection();
+
+		if(!$conn){
+			echo "DB connection error";
+		}
+
+		$sql = "insert into lab values('{$user['authorname']}', '{$user['contactnumber']}','{$user['username']}', '{$user['password']}')";
+		if(mysqli_query($conn, $sql)){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	function checkEmail($email)
+	{
+		$conn = dbConnection();
+		$sql = "select * from lab where email='{$email}'";
+		if(mysqli_query($conn, $sql)){
+			$result=mysqli_query($conn, $sql);
+			$user = mysqli_fetch_assoc($result);
+			if(empty($user)){
+			return false;
+			}
+			else
+			{
+				return true;
+			}
+		}else{
+			return false;
+		}
+
+	}
+
+
+	function update($user){
+		$conn = dbConnection();
+		if(!$conn){
+			echo "DB connection error";
+		}
+
+		$sql = "update lab set username='{$user['username']}', password='{$user['password']}', email='{$user['email']}' where id={$user['id']}";
+
+		if(mysqli_query($conn, $sql)){
+			return true;
+		}else{
+			return false;
+		}
+	}
+?>
